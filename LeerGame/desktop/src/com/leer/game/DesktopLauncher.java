eb5d5c18fd10cce9683925f56758f1fe92be9704
toberdas <tobiasijland@gmail.com>
2023-07-05 09:27:55 +0200

    Naam veranderd

--- LeerGame/desktop/src/com/leer/game/DesktopLauncher.java
+++ LeerGame/desktop/src/com/leer/game/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("Leer Game");
+		config.setTitle("Java Game");
 		new Lwjgl3Application(new LeerGame(), config);
 	}
 }
